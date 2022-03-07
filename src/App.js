09fe7259c55f76b689b66e9f08749b8d9720633b
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-07 12:35:42 +0900

    added neccessary paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+import PageNotFound from './components/404/404';
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/movie/:omdbID" element={<MovieDetails />} />
+          <Route element={PageNotFound} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/common/api/movieAPIKey.js
@@ -0,0 +1,1 @@
+export const APIKey = 'f9f9f9f9';
