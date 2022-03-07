a39c3df0ab051ee87ffbcaf3b064214b848cc9d0
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-07 13:08:06 +0900

    header code + minor styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Header.scss';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='header'>
+      <div className="logo">
+        <Link to='/'>
+          <img src="https://images.vexels.com/media/users/3/205178/isolated/lists/218500861b2b211836bafd36757a22aa-black-film-reel.png" alt="" />
+        </Link>
+      </div>
+      <div className="title">
+        <h1>Movie Night</h1>
+      </div>
+      <div className="user-image">
+        <img src="https://freesvg.org/img/abstract-user-flat-1.png" alt="user" />
+      </div>
+
     </div>
   );
 };
