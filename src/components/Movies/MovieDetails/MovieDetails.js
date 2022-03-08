5409e379e8d30138ad6d9becf025279cac37eb70
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-08 18:20:26 +0900

    fixing details page

--- src/components/Movies/MovieDetails/MovieDetails.js
+++ src/components/Movies/MovieDetails/MovieDetails.js
@@ -16,8 +16,55 @@ const MovieDetails = () => {
 
 
   return (
-    <div>
-      Movie Details
+    <div className='movie-section'>
+      <div className="section-left">
+        <div className="movie-title">
+          <h1>{data.Title}</h1>
+          <p>{data.Year}</p>
+        </div>
+        <div className="movie-rating">
+          <span>
+            IMDB rating: <h4>{data.imdbRating} <i className="fas fa-star"></i></h4>
+          </span>
+          <span>
+            Metascore: <h4>{data.Metascore} <i className="fas fa-star"></i></h4>
+          </span>
+          <span>
+            Runtime: <h4>{data.Runtime}</h4>
+          </span>
+          <span>
+            Country: <h4>{data.Country}</h4>
+          </span>
+          <span>
+            Language: <h4>{data.Language}</h4>
+          </span>
+          <span>
+            Actors: <h4>{data.Actors}</h4>
+          </span>
+          <span>
+            Director: <h4>{data.Director}</h4>
+          </span>
+          <span>
+            Writer: <h4>{data.Writer}</h4>
+          </span>
+          <span>
+            Awards: <h4>{data.Awards}</h4>
+          </span>
+          <span>
+            Production: <h4>{data.Production}</h4>
+          </span>
+          <span>
+            BoxOffice: <h4>{data.BoxOffice}</h4>
+          </span>
+          <div className='movie-plot'>
+            <h4>Plot:</h4>
+            <p>{data.Plot}</p>
+          </div>
+        </div>
+      </div>
+      <div className="section-right">
+        <img src={data.Poster} alt={data.Title} />
+      </div>
     </div>
   );
 };
