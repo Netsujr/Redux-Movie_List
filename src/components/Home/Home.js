d1a16813076b8df8867c96daa520bccbd989f817
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-08 09:13:01 +0900

    created slice, removed env

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,24 +2,13 @@ import React, { useEffect } from 'react';
 import './Home.scss';
 import MovieList from '../MovieList/MovieList';
 import { useDispatch } from 'react-redux';
-import { addMovies } from '../../features/movies/MovieSlice';
-import movieAPI from '../../common/api/movieAPI';
+import { fetchAsyncMovies } from '../../features/movies/MovieSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const movieSearch = 'Harry';
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieAPI
-        .get(`?apikey=${process.env.REACT_APP_MOVIE_API_KEY}&s=${movieSearch}&type=movie`)
-        .catch(error => {
-          console.log(error);
-        });
-      console.log(response.data);
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
+    dispatch(fetchAsyncMovies());
   }, []);
 
   return (
--- src/features/movies/MovieSlice.js
+++ src/features/movies/MovieSlice.js
@@ -1,8 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieAPI from "../../common/api/movieAPI";
 
 const initialState = {
   movies: {},
 };
+
+export const fetchAsyncMovies = createAsyncThunk("movies/fetchAsyncMovies",
+  async () => {
+    const movieSearch = 'Harry';
+    const response = await movieAPI
+      .get(`?apikey=${process.env.REACT_APP_MOVIE_API_KEY}&s=${movieSearch}&type=movie`)
+      .catch(error => {
+        console.log(error);
+      });
+    return response.data;
+  });
 
 const movieSlice = createSlice({
   name: "movies",
@@ -12,6 +24,26 @@ const movieSlice = createSlice({
       state.movies = payload;
     }
   },
+
+  extraReducers: {
+
+    [fetchAsyncMovies.pending]: () => {
+      console.log("Fetching Movies Pending");
+      // While the thunk is pending, we can dispatch additional actions
+      // but the state won't be affected until the thunk resolves.
+    },
+
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("Fetching Movies Success");
+      return { ...state, movies: payload };
+      // when the thunk is fulfilled, we update the state
+    },
+
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("Fetching Movies Rejected");
+    }
+
+  }
 });
 
 export const { addMovies } = movieSlice.actions;
