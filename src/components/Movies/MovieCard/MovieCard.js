af6ca50cac2cbd879e4482b1edc2fbbe84619597
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-11 14:06:38 +0900

    fixed no image issue

--- src/components/Movies/MovieCard/MovieCard.js
+++ src/components/Movies/MovieCard/MovieCard.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 
 const MovieCard = (props) => {
   const { data } = props;
+  const placeholderImg = 'https://icon-library.com/images/imdb-icon-png/imdb-icon-png-15.jpg';
+
   return (
     <div className='card-item'>
       <Link to={`/movie/${data.imdbID}`}>
         <div className='card-inner'>
           <div className='card-top'>
-            <img src={data.Poster} alt={data.Title} />
+            <img src={data.Poster === 'N/A' ? placeholderImg : data.Poster} alt={data.Title} />
           </div>
           <div className='card-bottom'>
             <div className="card-info">
